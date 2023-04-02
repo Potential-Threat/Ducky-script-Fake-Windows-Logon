5002401bfddaaef334a9dc50ea9ae3bffb06e4f8
CodingRabbi <50125131+CodingRabbi@users.noreply.github.com>
2023-04-02 01:54:14 -0400

    Update upload_2_db.ps1

--- upload_2_db.ps1
+++ upload_2_db.ps1
@@ -8,26 +8,26 @@ param (
 [string]$SourceFilePath
 ) 
 
-$db = "sl.Bbu_sEznesS6JQ7swX2yyz4U9KR8vuyD8H8FBVHTN1ykh4qU9IGCT8XpoAXiJWYx_AaDq8uxkhMfhzGCnpLofnBDJM4SL-c9acaJvV7XQaSveSqOUplDiJIVANLyt4x3eY-CPkiwaM0"
-
-$outputFile = Split-Path $SourceFilePath -leaf
-$TargetFilePath="/$outputFile"
-$arg = '{ "path": "' + $TargetFilePath + '", "mode": "add", "autorename": true, "mute": false }'
-$authorization = "Bearer " + $db
-$headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
-$headers.Add("Authorization", $authorization)
-$headers.Add("Dropbox-API-Arg", $arg)
-$headers.Add("Content-Type", 'application/octet-stream')
-
 try {
+    $db = "sl.Bbu_sEznesS6JQ7swX2yyz4U9KR8vuyD8H8FBVHTN1ykh4qU9IGCT8XpoAXiJWYx_AaDq8uxkhMfhzGCnpLofnBDJM4SL-c9acaJvV7XQaSveSqOUplDiJIVANLyt4x3eY-CPkiwaM0"
+
+    $outputFile = Split-Path $SourceFilePath -leaf
+    $TargetFilePath="/$outputFile"
+    $arg = '{ "path": "' + $TargetFilePath + '", "mode": "add", "autorename": true, "mute": false }'
+    $authorization = "Bearer " + $db
+    $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+    $headers.Add("Authorization", $authorization)
+    $headers.Add("Dropbox-API-Arg", $arg)
+    $headers.Add("Content-Type", 'application/octet-stream')
+
     Invoke-RestMethod -Uri https://content.dropboxapi.com/2/files/upload -Method Post -InFile $SourceFilePath -Headers $headers
     return $true
 }
 catch {
-    $errorMessage = $_.Exception.Response.GetResponseStream()
-    $reader = New-Object System.IO.StreamReader($errorMessage)
-    $responseBody = $reader.ReadToEnd()
-    Write-Host "Error message: $($_.Exception.Response.StatusCode) - $responseBody"
+    $errorLog = "ErrorLog.txt"
+    $errorMessage = $_.Exception | Format-List -Force | Out-String
+    $errorMessage | Out-File $errorLog -Append
+    Write-Host "Error details saved to $errorLog"
     return $false
 }
 }
